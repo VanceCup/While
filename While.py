6dd0b984b8c9e5fab926304944eea9de30d7e4c1
VanceCup <maksim_ponomarev2003@mail.ru>
2024-08-22 00:45:41 +0500

    While

---
+++ While.py
@@ -0,0 +1,36 @@
+MyList = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+Zero = 0
+(len(MyList))
+while len(MyList):
+    if MyList[0] > Zero:
+        print(MyList[0])
+    else:
+        break
+    if MyList[1] > Zero:
+        print(MyList[1])
+    else:
+        break
+    if MyList[2] > Zero:
+        print(MyList[2])
+    else:
+        break
+    if MyList[3] > Zero:
+        print(MyList[3])
+    else:
+        break
+    if MyList[4] > Zero:
+        print(MyList[4])
+    else:
+        break
+    if MyList[5] > Zero:
+        print(MyList[5])
+    else:
+        break
+    if MyList[6] > Zero:
+        print(MyList[6])
+    else:
+        break
+    if MyList[7] > Zero:
+        print(MyList[7])
+    else:
+        break
